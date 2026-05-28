b94de43efe697d82afdec92cec5a45d332a75256
miguel robledo <stmiguelangelgarciarobledo@gmail.com>
2026-05-27 22:34:42 -0500

    Agrego base de datos de financial-service

--- tourist-catalog-service/src/main/java/com/hernandolopera/operation_servicio/controlador/ControladorPaqueteTuristico.java
+++ tourist-catalog-service/src/main/java/com/hernandolopera/operation_servicio/controlador/ControladorPaqueteTuristico.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/paquetes")
+@RequestMapping("/api/paquetes")
 public class ControladorPaqueteTuristico {
     private final ServicioPaqueteTuristico servicio;
 
